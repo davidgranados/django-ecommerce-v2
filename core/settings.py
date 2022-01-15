ee27da7f287110c2a56891231faab2d47b0c4b2c
davidgranados-celeritech <david.granados@celeritech.biz>
2022-01-14 19:41:48 -0500

    Django Project E-commerce v2 Part 2 - Pahse 3

--- core/settings.py
+++ core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     # Local apps
     "dashboard",
     "inventory",
+    "demo",
 ]
 
 MIDDLEWARE = [
--- tests/factories.py
+++ tests/factories.py
@@ -15,4 +15,27 @@ class CategoryFactory(factory.django.DjangoModelFactory):
     slug = factory.Sequence(lambda n: "cat-slug-%d" % n)
 
 
+class ProductFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Product
+
+    web_id = factory.Sequence(lambda n: "web_id_%d" % n)
+    slug = factory.Sequence(lambda n: "prod-slug-%d" % n)
+    name = factory.Sequence(lambda n: "product name %d" % n)
+    description = fake.text()
+    is_active = True
+    created_at = fake.date_this_month()
+    updated_at = fake.date_this_month()
+
+    @factory.post_generation
+    def category(self, create, extracted, **kwargs):
+        if not create or not extracted:
+            return
+
+        if extracted:
+            for cat in extracted:
+                self.category.add(cat)
+
+
 register(CategoryFactory)
+register(ProductFactory)
